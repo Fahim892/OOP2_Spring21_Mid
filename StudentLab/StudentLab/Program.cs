3405280c9c91557893f6a8d3554daf7f34098c99
Fahim892 <Ahsanhabibfahim420@gmail.com>
2021-02-07 13:58:44 +0600

    student lab task

---
+++ StudentLab/StudentLab/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
