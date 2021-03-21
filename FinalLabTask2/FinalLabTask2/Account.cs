afa0c4798fef36eb62c19136c5b952b4a349f661
Fahim892 <Ahsanhabibfahim420@gmail.com>
2021-03-21 14:09:12 +0600

    FinalLabtask2

---
+++ FinalLabTask2/FinalLabTask2/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalLabTask2
+{
+
+    abstract class Account
+    {
+
+        public string AccountName { get; set; }
+        public string AccountNo { get; }
+        public double Balance { get; set; }
+        public Account() { }
+
+        public Account(string accountName, string accountNo, double balance)
+        {
+            AccountName = accountName;
+            AccountNo = accountNo;
+            Balance = balance;
+        }
+
+        abstract public void ShowInfo();
+
+
+        abstract public void Deposit(double amount);
+
+        abstract public void Withdraw(double amount);
+
+        abstract public void Transfer(Account a, double amount);
+
+
+
+    }
+}
---
+++ FinalLabTask2/FinalLabTask2/Overdraft.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalLabTask2
+
+{
+    class OverDraft : Account
+    {
+        public int Limit { get; set; }
+        public OverDraft()
+        {
+
+        }
+        public OverDraft(string accountName, string accountNo, double balance, int limit) : base(accountName, accountNo, balance)
+        {
+            Limit = limit;
+        }
+
+        public override void Deposit(double amount)
+        {
+            Balance = Balance + Limit + amount;
+            Console.WriteLine("Your acc is credited by {0} to self and updated  balance is {1} ", amount, Balance);
+        }
+        public override void Withdraw(double amount)
+        {
+            if (amount <= (Balance + Limit))
+            {
+                Balance = Balance - amount;
+                Console.WriteLine("Your acc is debited by {0} and updated  balance is {1} ", amount, Balance);
+            }
+            else
+            {
+
+                Console.WriteLine("Insuffient Balance maximum amount you can withdraw is:" + (Balance + Limit));
+            }
+        }
+        public override void Transfer(Account a, double amount)
+        {
+            if (amount <= (Balance + Limit))
+            {
+                Balance = Balance - amount;
+
+
+                Console.WriteLine("You account is debited by {0} new balance {1} and credited to  account: {2} ({3})", amount, Balance, a.AccountName, a.AccountNo);
+            }
+            else
+            {
+                Console.WriteLine("Insuffient Balance maximum amount you can transfer is:" + (Balance + Limit));
+            }
+
+        }
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine("Name: " + AccountName);
+            Console.WriteLine("Acc No: " + AccountNo);
+
+        }
+
+
+    }
+
+}
